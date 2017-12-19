26d68c07ddeb4484c1faca8e3bca7fc96c8c743e
Jeff Setter <setter@stanford.edu>
2017-12-18 18:01:44 -0800

    revert to single apt-get (build essential uses 4.8)

--- install.sh
+++ install.sh
@@ -5,17 +5,7 @@ sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install g++-4.9 gcc-4.9
 
-sudo apt-get install verilator
-sudo apt-get install build-essential
-sudo apt-get install clang
-sudo apt-get install libedit-dev
-sudo apt-get install libpng12-dev
-sudo apt-get install csh
-sudo apt-get install libgmp3-dev
-sudo apt-get install git
-sudo apt-get install zlib1g
-sudo apt-get install zlib1g-dev
-sudo apt-get install graphviz-dev 
+sudo apt-get install verilator build-essential clang libedit-dev libpng12-dev csh libgmp3-dev git zlib1g zlib1g-dev graphviz-dev 
 
 if [[ -z "${TRAVIS_BUILD_DIR}" ]]; then
     # Halide_CoreIR/test/scripts/install_travis.sh is known to use this
